75eaee98df89b8a8110e3a7c915252ce8fe0a8f3
RICARDO VINICIUS DE SOUZA SIQUEIRA <ricardo_v_siqueira@estudante.sesisenai.org.br>
2026-06-22 21:11:02 -0300

    trilha finalizada

--- praticaExpress/index.js
+++ praticaExpress/index.js
@@ -18,17 +18,41 @@ app.get('/users/:name', (req, res) =>{
 });
 
 app.post('/cars/', (req, res) => {
-    let name = req.body.name;
+    let name = req.body && req.body.name;
+
+    if(!name){
+        return res.status(400).json({erro: 'envie name em x-www-form-urlencoded'});
+    }
     carros[(carros.length)] = name;
     return res.json([carros[(carros.length - 1)]]);
 });
 
 app.get('/cars/:id', (req, res) =>{
     let id = req.params.id;
+
+    if(isNaN(id)){
+        id = carros.indexOf(id);
+    }
+
+    if(id < 0 || carros[id] === undefined){
+        return res.status(404).json({erro: 'carro não encontrado'});
+    }
     return res.json([carros[id]])
 });
 
+app.put('/cars/uptade/:id', (req, res)=>{
+    let name = req.body.name;
+    carros[req.params.id] = name;
+    return res.json(carros[req.params.id]);
+});
+
+app.delete('/cars/delete/:id', (req, res)=>{
+    let id = req.params.id;
+    carros[id] = null;
+    return res.json(carros[id]);
+})
 
 app.listen(3000, () => 
 console.log('Servidor iniciado na porta 3000')
 );
+
